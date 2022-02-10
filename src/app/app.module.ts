b6db58a2e41f79d3e7f285975bf0efb8c9a06ab3
rasadulalam <rashed464@outlook.com>
2022-02-10 09:47:34 +0600

    First release

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import {FooterComponent} from './layout/footer/footer.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,FooterComponent
   ],
   imports: [
     BrowserModule
