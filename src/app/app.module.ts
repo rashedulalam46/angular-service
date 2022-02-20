4e66de31b729f1184bef7b4965cbaed75567712b
rasadulalam <rashed464@outlook.com>
2022-02-20 11:33:10 +0600

    code change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {FooterComponent} from './layout/footer/footer.component'
 
-
-
 @NgModule({
   declarations: [
     AppComponent,FooterComponent
